1ae04e6ebed0d573e4381c26d060fbd77c61e46f
Mishalr7 <iammishalanzan@gmail.com>
2026-03-02 17:05:37 +0530

    server.js updated

--- server/server.js
+++ server/server.js
@@ -14,12 +14,15 @@ app.use(cors());
 app.use(express.json());
 app.use("/api/auth", authRoutes);
 app.use("/api/transactions", transactionRoutes);
-app.use("/api/transactions", transactionRoutes);
 
 mongoose
   .connect(process.env.MONGO_URI)
-  .then(() => console.log("MongoDB connected"))
-  .catch(err => console.error(err));
+  .then(() => {
+    console.log("✅ MongoDB connected");
+  })
+  .catch((err) => {
+    console.error("❌ MongoDB connection error:", err);
+  });
 
 app.listen(5000, () => {
   console.log("Server running on http://localhost:5000");
