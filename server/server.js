5023bc67e66b8601edd1ded68f4dafb9eea1b9ad
Mishalr7 <iammishalanzan@gmail.com>
2026-03-03 23:58:06 +0530

    updated server.js

--- server/server.js
+++ server/server.js
@@ -5,25 +5,31 @@ import dotenv from "dotenv";
 import authRoutes from "./routes/auth.routes.js";
 import transactionRoutes from "./routes/transaction.routes.js";
 
-
-
 dotenv.config();
 
 const app = express();
-app.use(cors());
+
+app.use(cors({
+  origin: process.env.CLIENT_URL,
+  credentials: true
+}));
+
 app.use(express.json());
+
 app.use("/api/auth", authRoutes);
 app.use("/api/transactions", transactionRoutes);
 
 mongoose
   .connect(process.env.MONGO_URI)
-  .then(() => {
-    console.log("✅ MongoDB connected");
-  })
-  .catch((err) => {
-    console.error("❌ MongoDB connection error:", err);
-  });
+  .then(() => console.log("✅ MongoDB connected"))
+  .catch((err) => console.error("❌ MongoDB connection error:", err));
 
-app.listen(5000, () => {
-  console.log("Server running on http://localhost:5000");
+app.get("/", (req, res) => {
+  res.json({ status: "FinanceTracker API running" });
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
